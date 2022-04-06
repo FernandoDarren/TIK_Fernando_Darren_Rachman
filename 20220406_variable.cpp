d5c2ae01b700ce6b3c2d4af800e0043415124061
FernandoDarren <103085866+FernandoDarren@users.noreply.github.com>
2022-04-05 20:08:49 -0700

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,17 @@
+//header
+#include <iostream>  //input outpout stream
+using namespace std;
+
+int main(){//begin of function
+    string nama; //deklarasi variabel
+    float angka;
+    float angka2;
+    
+    nama = "Fernando Darren Rachman"; //masukkan value ke dalam variabel
+    angka = 10.21;
+    angka2 = 3.14;
+    
+    cout<<nama<<endl;//menampilkan value variabel
+    cout<<angka+angka2;
+
+}//end of function
