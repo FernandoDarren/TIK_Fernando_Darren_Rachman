06eb1eaeebf2a3497d51b9da874172f993265cb2
FernandoDarren <103085866+FernandoDarren@users.noreply.github.com>
2022-04-05 20:27:37 -0700

    Create 20220406_inputoutput.cpp

---
+++ 20220406_inputoutput.cpp
@@ -0,0 +1,9 @@
+//header
+#include <iostream>  //input outpout stream
+using namespace std;
+
+int main(){//begin of function
+    string nama;
+    cout<<"masukkan nama: "; cin>>nama;
+    cout<<"nama anda: "<<nama;
+}//end of function
