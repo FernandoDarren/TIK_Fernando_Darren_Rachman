3e311a18f85bef31aa08510fe77789d1f8d137c7
FernandoDarren <103085866+FernandoDarren@users.noreply.github.com>
2022-05-11 10:02:29 +0800

    Create 20220511_array.cpp

---
+++ 20220511_array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama[] = {
+        "David",  //0
+        "Kabila", //1
+        "Bryan", //2
+        "Sydney", //3
+        "Gracia",
+        "Keysie",
+        "Yoel"
+    };
+    for (int i = 0; i<7 ; i ++){
+        cout<<nama[i]<<endl;
+    }
+    return 0;
+}
